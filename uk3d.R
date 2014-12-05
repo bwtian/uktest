251231c64fe25f329a7ebfdb6428db09ca6484fe
lab1 <bwtian@gmail.com>
2014-12-05 11:12:41 +0900

    / ‘uk3d.R’

--- uk3d.R
+++ uk3d.R
@@ -1,8 +1,9 @@
 ## Thank you very much for answer
-# 1. I think the Z is not the problem, because I use a regular grid
+# 1. Dear, edzer, I think the Z is not the problem, because I use a regular grid
 # 2. With a nugget of 0.0001 as suggested by Jon Skoien have decreaed the value a little
 ## from 16 to 14 but still quite large
-# 3. nmax have a big influces to the predicted value
+# 3. nmax have a big affect to the predicted value
+# 4. Maybe the problem is the outliers, i.e. at shallow depth have high temperature of t~z
 setwd("uktest")
 spdf  <- readRDS("spdf")
 grid  <- readRDS("grid")
@@ -14,7 +15,6 @@ uk.vgm <- variogram(logt ~ z, spdf,
                     boundaries = c(seq(100,1000,100), seq(2000,45000,5000)))
 plot(uk.vgm)
 
-
 ##
 uk.eye1  <- vgm(psill = 0.155,  model = "Gau",  range=700,  nugget=0.0001)
 uk.eye   <- vgm(psill = 0.125,  model = "Sph",  range=35000,  nugget=0,  add.to=uk.eye1)
@@ -22,8 +22,7 @@ plot(uk.vgm, model = uk.eye, plot.numbers = TRUE)
 
 summary(spdf)
 summary(grid)
-
 logt.uk <- krige(log(t)~z, spdf, grid, model = uk.eye, nmax = 10)
-## predicedt and ovserved should be same
+## predicedt and ovserved should be same, max should aroud 6
 summary((logt.uk$var1.pred))
 summary((spdf$logt))
