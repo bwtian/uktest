330a37fd67bfc6aafc0223da9257707c01d0059f
lab1 <bwtian@gmail.com>
2014-12-05 10:46:36 +0900

    / ‘uk3d.R’

--- uk3d.R
+++ uk3d.R
@@ -2,6 +2,14 @@ uk.vgm <- variogram(logt ~ z, spdf,
                     boundaries = c(seq(100,1000,100), seq(2000,45000,5000)))
 plot(uk.vgm)
 
+
 show.vgms()
 uk.eye1  <- vgm(psill = 0.155,  model = "Gau",  range=700,  nugget=0.0001)
 uk.eye   <- vgm(psill = 0.125,  model = "Sph",  range=35000,  nugget=0,  add.to=uk.eye1)
+plot(uk.vgm, model = uk.eye, plot.numbers = TRUE)
+
+summary(spdf)
+summary(grid)
+logt.uk <- krige(log(t)~z, spdf, grid, model = uk.eye, nmax = 10)
+summary((spdf$logt))
+summary((logt.uk$var1.pred))
